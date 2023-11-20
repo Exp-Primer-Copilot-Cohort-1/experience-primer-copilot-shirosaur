929e7280e2899155354794f0dbbdaac18b2622db
dominik <94128595+shirosaur@users.noreply.github.com>
2023-11-20 13:30:03 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,33 +1,71 @@
-// create web server
+// create web server to handle request and send response
 // import http module
 const http = require('http');
 
-// import fs module
-const fs = require('fs');
+// import url module
+const url = require('url');
+
+// import querystring module
+const querystring = require('querystring');
+
+// create server
+http.createServer(function(req, res) {
+    // get the query string
+    const query = url.parse(req.url).query;
+    // parse the query string
+    const params = querystring.parse(query);
+
+    // set the response header
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    if ('firstname' in params && 'lastname' in params) {
+        // response with firstname and lastname
+        res.write('Your name is ' + params['firstname'] + ' ' + params['lastname']);
+    } else {
+        // response without firstname and lastname
+        res.write('Your name is undefined');
+    }
+    res.end();
+}).listen(8081);
+
+// print the message
+console.log('Server running at http://');
+
+// Path: hello.js
+// create web server to handle request and send response
+// import http module
+const http = require('http');
 
 // import url module
 const url = require('url');
 
 // create server
 http.createServer(function(req, res) {
-    // get the path name
-    const pathname = url.parse(req.url).pathname;
-    // print the path name
-    console.log("Request for " + pathname + " received.");
+    // get the query string
+    const query = url.parse(req.url, true).query;
+    // get the name
+    const name = query.name;
 
-    // read the file content
-    fs.readFile(pathname.substr(1), function(err, data) {
-        if (err) {
-            console.log(err);
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-        } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(data.toString());
-        }
-        res.end();
-    });
+    // set the response header
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    // response with name
+    res.write('Hello ' + name);
+    res.end();
 }).listen(8081);
 
 // print the message
 console.log('Server running at http://');
-// wait it works now
+
+// Path: index.html
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Node.js GET Request</title>
+    <meta charset="utf-8"></meta>
+</head>
+
+<body>
+    <form action="http://"></form>
+</body>
+</html>
